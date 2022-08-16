bf90bf33ad3da083ce3bc726f6cd19363ec7ff60
Geo <geovanna@ifut.com.br>
2022-08-16 19:19:37 -0300

    feat: pokemon service loading data from api

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input } from '@angular/core';
-import { Pokemon } from 'src/_model/Pokemon';
+import {
+  getPokemonImage,
+  getPokemonNumber,
+  Pokemon
+} from '../../_model/Pokemon';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -7,14 +11,10 @@ import { Pokemon } from 'src/_model/Pokemon';
   styleUrls: ['./pokemon-card.component.sass']
 })
 export class PokemonCardComponent {
-  @Input('pokemon')
+  @Input()
   public pokemon: Pokemon;
 
-  public leadingZero(str: number | string, size = 3): string {
-    let s = String(str);
-    while (s.length < (size || 2)) {
-      s = '0' + s;
-    }
-    return s;
-  }
+  public getPokemonImage = getPokemonImage;
+
+  public getPokemonNumber = getPokemonNumber;
 }
